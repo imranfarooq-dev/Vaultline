28973b29db7bbd7bc444f6e9c145ecf8a162fa15
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-11-05 11:01:30 +0500

    feat(app): add bootstrap()

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import 'reflect-metadata';
+import { ConsoleLogger, Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { configureApp } from './app.setup';
+import { AppModule } from './app.module';
+import { WorkerModule } from './worker.module';
+
+async function bootstrap(): Promise<void> {
+  const mode = process.env.APP_MODE === 'worker' ? 'worker' : 'api';
+  const logger = new ConsoleLogger({ json: process.env.LOG_FORMAT === 'json', prefix: mode === 'api' ? 'BankAPI' : 'BankWorker' });
+}
