7b6df4ee896c23026ec52a8df00e34395c034b1c
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-11-06 11:40:44 +0500

    feat(common): add DomainError

---
+++ src/common/errors/domain.errors.ts
@@ -0,0 +1,13 @@
+/**
+ * Business errors are plain classes with no HTTP knowledge.
+ * The DomainExceptionFilter translates them into HTTP responses.
+ */
+export abstract class DomainError extends Error {
+  abstract readonly code: string;
+  abstract readonly httpStatus: number;
+
+  constructor(message: string, readonly details?: Record<string, unknown>) {
+    super(message);
+    this.name = new.target.name;
+  }
+}
