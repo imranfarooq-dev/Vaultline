9cb0b37c73fa87555502311d4cbb9ffcf70598be
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-11-07 21:28:34 +0500

    feat(database): add MIGRATIONS

---
+++ src/database/data-source.ts
@@ -0,0 +1,6 @@
+import 'reflect-metadata';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ALL_ENTITIES } from './entities';
+import { InitialSchema1726000000000 } from './migrations/1726000000000-InitialSchema';
+
+export const MIGRATIONS = [InitialSchema1726000000000];
