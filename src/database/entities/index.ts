268f463fb2f29045124c05ee5a6bce4396f282ee
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-11-08 18:13:44 +0500

    feat(database): add index.ts

---
+++ src/database/entities/index.ts
@@ -0,0 +1,6 @@
+import { AccountEntity } from './account.entity';
+import { LedgerEntryEntity } from './ledger-entry.entity';
+import { LoanApplicationEntity } from './loan-application.entity';
+import { NotificationEntity } from './notification.entity';
+
+export * from './account.entity';
