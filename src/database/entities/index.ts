e4c7136aae0bba18a5583763862122714564452c
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-11-09 17:09:46 +0500

    feat(database): add ALL_ENTITIES

--- src/database/entities/index.ts
+++ src/database/entities/index.ts
@@ -4,3 +4,8 @@ import { LoanApplicationEntity } from './loan-application.entity';
 import { NotificationEntity } from './notification.entity';
 
 export * from './account.entity';
+export * from './ledger-entry.entity';
+export * from './loan-application.entity';
+export * from './notification.entity';
+
+export const ALL_ENTITIES = [AccountEntity, LedgerEntryEntity, LoanApplicationEntity, NotificationEntity];
