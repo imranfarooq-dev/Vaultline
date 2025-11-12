a6192dfd8eaf6af4bd2b15e960bd73c8116cec1b
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-11-12 17:37:20 +0500

    feat(database): add createdAt and updatedAt to AccountEntity

--- src/database/entities/account.entity.ts
+++ src/database/entities/account.entity.ts
@@ -50,4 +50,10 @@ export class AccountEntity {
 
   @VersionColumn()
   version: number;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamptz' })
+  updatedAt: Date;
 }
