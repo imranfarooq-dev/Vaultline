1de6f808f6c290587814fe1441f29b588cf93ac4
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-11-20 15:24:32 +0500

    test(core): always returns the very same instance

--- src/modules/core/core.patterns.spec.ts
+++ src/modules/core/core.patterns.spec.ts
@@ -3,4 +3,8 @@ import { ReferenceNumberGenerator } from './reference-number.singleton';
 
 describe('Singleton: ReferenceNumberGenerator', () => {
   beforeEach(() => ReferenceNumberGenerator.resetForTesting());
+
+  it('always returns the very same instance', () => {
+    expect(ReferenceNumberGenerator.getInstance()).toBe(ReferenceNumberGenerator.getInstance());
+  });
 });
