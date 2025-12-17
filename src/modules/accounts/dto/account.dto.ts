2084cad3a47a4b0916f247bc9da4983c9cd56414
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-12-17 15:26:34 +0500

    feat(accounts): add OpenAccountDto

---
+++ src/modules/accounts/dto/account.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsIn, IsInt, IsOptional, IsString, Length, Min } from 'class-validator';
+import { AccountType } from '../../../database/entities';
+
+export class OpenAccountDto {
+  @ApiProperty({ example: 'Ayesha Khan' })
+  @IsString()
+  @Length(2, 120)
+  ownerName: string;
+}
