490df284cf27fe301ff3aff540c5e4510c0044a7
imranfarooq-dev <imranfarooq.dev@gmail.com>
2025-12-24 12:04:12 +0500

    feat(transactions): add PostingOptions type

---
+++ src/modules/transactions/ledger.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource, EntityManager, In, MoreThanOrEqual } from 'typeorm';
+import { BusinessRuleError, NotFoundError } from '../../common/errors/domain.errors';
+import { AccountEntity, LedgerEntryEntity, LedgerEntryType } from '../../database/entities';
+import { ReferenceNumberGenerator } from '../core/reference-number.singleton';
+import { DomainEventBus } from '../messaging/domain-event-bus.observer';
+import { createEvent, DomainEvent, EventTypes } from '../messaging/domain-events';
+import { buildTransactionValidationChain } from './validation/transaction-validation.chain';
+
+export interface PostingOptions {
+  description?: string;
+  type?: LedgerEntryType;
+}
