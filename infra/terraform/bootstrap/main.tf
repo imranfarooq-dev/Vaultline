59d0da9e8244b962a98ed6df3b1950ab683caddd
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-05-14 11:40:04 +0500

    feat(infra/bootstrap): add aws_s3_bucket_public_access_block.state

--- infra/terraform/bootstrap/main.tf
+++ infra/terraform/bootstrap/main.tf
@@ -51,3 +51,11 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "state" {
     }
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "state" {
+  bucket                  = aws_s3_bucket.state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
