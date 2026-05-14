cbe1a81146cef0a53446dd84986e84bd730990bf
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-05-14 14:13:36 +0500

    feat(infra/bootstrap): add bucket_name output

--- infra/terraform/bootstrap/main.tf
+++ infra/terraform/bootstrap/main.tf
@@ -59,3 +59,7 @@ resource "aws_s3_bucket_public_access_block" "state" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+output "bucket_name" {
+  value = aws_s3_bucket.state.bucket
+}
