99cd48c7044414d63b7f8be6445c21e8ce78b98a
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-05-19 20:41:01 +0500

    feat(infra/eks): add node_security_group_id output

--- infra/terraform/modules/eks/outputs.tf
+++ infra/terraform/modules/eks/outputs.tf
@@ -5,3 +5,8 @@ output "cluster_name" {
 output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
+
+output "node_security_group_id" {
+  description = "Pods use the node security group, so data stores allow traffic from it."
+  value       = module.eks.node_security_group_id
+}
