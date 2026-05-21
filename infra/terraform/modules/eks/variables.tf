03297112763a91d699188e7ca326ef442376ff53
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-05-21 17:40:31 +0500

    feat(infra/eks): add api_allowed_cidrs variable

--- infra/terraform/modules/eks/variables.tf
+++ infra/terraform/modules/eks/variables.tf
@@ -14,3 +14,9 @@ variable "vpc_id" {
 variable "private_subnet_ids" {
   type = list(string)
 }
+
+variable "api_allowed_cidrs" {
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+  description = "Who may reach the Kubernetes API. Set to your office/VPN IP in real environments."
+}
