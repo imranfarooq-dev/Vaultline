903b2f7b446e1771bbc2a9e4645d06ed2edd17a1
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-06-02 23:08:31 +0500

    feat(infra/msk): add aws_msk_cluster.this

--- infra/terraform/modules/msk/main.tf
+++ infra/terraform/modules/msk/main.tf
@@ -43,3 +43,48 @@ resource "aws_cloudwatch_log_group" "broker" {
   name              = "/aws/msk/${var.name}"
   retention_in_days = 7
 }
+
+resource "aws_msk_cluster" "this" {
+  cluster_name           = var.name
+  kafka_version          = var.kafka_version
+  number_of_broker_nodes = var.broker_count # must be a multiple of the number of subnets
+
+  broker_node_group_info {
+    instance_type   = var.instance_type
+    client_subnets  = slice(var.subnet_ids, 0, var.broker_count)
+    security_groups = [aws_security_group.msk.id]
+
+    storage_info {
+      ebs_storage_info {
+        volume_size = var.volume_size_gb
+      }
+    }
+  }
+
+  configuration_info {
+    arn      = aws_msk_configuration.this.arn
+    revision = aws_msk_configuration.this.latest_revision
+  }
+
+  encryption_info {
+    encryption_in_transit {
+      client_broker = "TLS"
+      in_cluster    = true
+    }
+  }
+
+  # TLS encryption without client authentication keeps the app configuration simple.
+  # Production upgrade: IAM or SASL/SCRAM authentication.
+  client_authentication {
+    unauthenticated = true
+  }
+
+  logging_info {
+    broker_logs {
+      cloudwatch_logs {
+        enabled   = true
+        log_group = aws_cloudwatch_log_group.broker.name
+      }
+    }
+  }
+}
