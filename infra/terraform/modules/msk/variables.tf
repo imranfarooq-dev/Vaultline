67aad0ab942655ce49c6994108e7b4fd95addfa4
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-06-03 23:14:02 +0500

    feat(infra/msk): add broker_count variable

--- infra/terraform/modules/msk/variables.tf
+++ infra/terraform/modules/msk/variables.tf
@@ -18,3 +18,13 @@ variable "kafka_version" {
   type    = string
   default = "3.6.0"
 }
+
+variable "broker_count" {
+  type    = number
+  default = 2
+
+  validation {
+    condition     = var.broker_count >= 2 && var.broker_count <= 3
+    error_message = "Use 2 brokers (dev) or 3 brokers (prod), one per subnet."
+  }
+}
