209b5e81abf6db923e5c7b4449e14e8273dd0505
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-06-06 23:30:49 +0500

    feat(infra/live): add environment variable

---
+++ infra/terraform/live/variables.tf
@@ -0,0 +1,7 @@
+variable "environment" {
+  type = string
+  validation {
+    condition     = contains(["dev", "staging", "prod"], var.environment)
+    error_message = "environment must be dev, staging or prod."
+  }
+}
