03a7e8352e451bf2ed0ce3fd5a4b0f45db3847ae
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-06-07 19:14:40 +0500

    feat(infra/live): add configure_kubectl output

--- infra/terraform/live/outputs.tf
+++ infra/terraform/live/outputs.tf
@@ -5,3 +5,7 @@ output "region" {
 output "cluster_name" {
   value = module.eks.cluster_name
 }
+
+output "configure_kubectl" {
+  value = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
+}
