9f44df4012ef915135b3dbafacb83329dea57427
imranfarooq-dev <imranfarooq.dev@gmail.com>
2026-06-08 14:09:20 +0500

    feat(infra/live): add ecr_repository_url output

--- infra/terraform/live/outputs.tf
+++ infra/terraform/live/outputs.tf
@@ -9,3 +9,7 @@ output "cluster_name" {
 output "configure_kubectl" {
   value = "aws eks update-kubeconfig --region ${var.region} --name ${module.eks.cluster_name}"
 }
+
+output "ecr_repository_url" {
+  value = module.ecr.repository_url
+}
